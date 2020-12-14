81b2106ed2ac9931bb274c6981d89aa6c166ead4
10matcho27 <ryo.1027.smile@gmail.com>
2020-12-14 17:27:01 +0900

    can't submit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-31.times do |i|
+30.times do |i|
     Month.create(id:i+1,date:1)
 end
+Month.create(id:31,date:-1)
