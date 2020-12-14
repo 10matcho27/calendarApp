fdd5ff0e420bb52a74251963e0c20c7eb4f6337c
10matcho27 <ryo.1027.smile@gmail.com>
2020-12-14 17:24:59 +0900

    can't submit

--- db/migrate/20201212044055_create_users.rb
+++ db/migrate/20201212044055_create_users.rb
@@ -1,7 +1,7 @@
 class AddColumnToDates < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.boolean :dates, array: true default: []
+      t.boolean :dates
     end
   end
 end
